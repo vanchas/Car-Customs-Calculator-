0a7dcd660532a30101ba57f274cf0191499b7c3d
Ivan <vanchas@protonmail.com>
2020-06-02 10:14:50 +0300

    separate js files

---
+++ js/citiesIAAI.js
@@ -0,0 +1,1046 @@
+const citiesIAAI = [
+  {
+    "city": "Birmingham",
+    "port": "ga",
+    "state": "al",
+    "price": "291"
+  },
+  {
+    "city": "Dothan",
+    "port": "ga",
+    "state": "al",
+    "price": "291"
+  },
+  {
+    "city": "Huntsville",
+    "port": "ga",
+    "state": "al",
+    "price": "291"
+  },
+  {
+    "city": "Fayetteville",
+    "port": "ga",
+    "state": "ar",
+    "price": "461"
+  },
+  {
+    "city": "Little Rock",
+    "port": "ga",
+    "state": "ar",
+    "price": "421"
+  },
+  {
+    "city": "Phoenix",
+    "port": "ca",
+    "state": "az",
+    "price": "560"
+  },
+  {
+    "city": "Tucson",
+    "port": "ca",
+    "state": "az",
+    "price": "570"
+  },
+  {
+    "city": "ACE Carson",
+    "port": "ca",
+    "state": "ca",
+    "price": "420"
+  },
+  {
+    "city": "ACE Perris",
+    "port": "ca",
+    "state": "ca",
+    "price": "583"
+  },
+  {
+    "city": "ACE Perris 2",
+    "port": "ca",
+    "state": "ca",
+    "price": "583"
+  },
+  {
+    "city": "Anaheim",
+    "port": "ca",
+    "state": "ca",
+    "price": "370"
+  },
+  {
+    "city": "Colton",
+    "port": "ca",
+    "state": "ca",
+    "price": "410"
+  },
+  {
+    "city": "East Bay",
+    "port": "ca",
+    "state": "ca",
+    "price": "500"
+  },
+  {
+    "city": "Fontana",
+    "port": "ca",
+    "state": "ca",
+    "price": "360"
+  },
+  {
+    "city": "Fremont",
+    "port": "ca",
+    "state": "ca",
+    "price": "510"
+  },
+  {
+    "city": "Fresno",
+    "port": "ca",
+    "state": "ca",
+    "price": "450"
+  },
+  {
+    "city": "High Desert",
+    "port": "ca",
+    "state": "ca",
+    "price": "460"
+  },
+  {
+    "city": "Los Angeles",
+    "port": "ca",
+    "state": "ca",
+    "price": "320"
+  },
+  {
+    "city": "North Hollywood",
+    "port": "ca",
+    "state": "ca",
+    "price": "415"
+  },
+  {
+    "city": "Riverside",
+    "port": "ca",
+    "state": "ca",
+    "price": "1128"
+  },
+  {
+    "city": "Sacramento",
+    "port": "ca",
+    "state": "ca",
+    "price": "530"
+  },
+  {
+    "city": "San Diego",
+    "port": "ca",
+    "state": "ca",
+    "price": "400"
+  },
+  {
+    "city": "Denver",
+    "port": "nj",
+    "state": "co",
+    "price": "540"
+  },
+  {
+    "city": "Denver East",
+    "port": "nj",
+    "state": "co",
+    "price": "540"
+  },
+  {
+    "city": "Western Colorado",
+    "port": "nj",
+    "state": "co",
+    "price": "940"
+  },
+  {
+    "city": "Hartford",
+    "port": "nj",
+    "state": "ct",
+    "price": "210"
+  },
+  {
+    "city": "Hartford-South",
+    "port": "nj",
+    "state": "ct",
+    "price": "210"
+  },
+  {
+    "city": "New Castle",
+    "port": "nj",
+    "state": "de",
+    "price": "274"
+  },
+  {
+    "city": "Clearwater",
+    "port": "fl",
+    "state": "fl",
+    "price": "152"
+  },
+  {
+    "city": "Fort Myers",
+    "port": "fl",
+    "state": "fl",
+    "price": "113"
+  },
+  {
+    "city": "Fort Piece",
+    "port": "fl",
+    "state": "fl",
+    "price": "150"
+  },
+  {
+    "city": "Jacksonville",
+    "port": "fl",
+    "state": "fl",
+    "price": "158"
+  },
+  {
+    "city": "Miami",
+    "port": "fl",
+    "state": "fl",
+    "price": "44"
+  },
+  {
+    "city": "Miami-North",
+    "port": "fl",
+    "state": "fl",
+    "price": "95"
+  },
+  {
+    "city": "Orlando",
+    "port": "fl",
+    "state": "fl",
+    "price": "136"
+  },
+  {
+    "city": "Orlando-North",
+    "port": "fl",
+    "state": "fl",
+    "price": "131"
+  },
+  {
+    "city": "Pensacola",
+    "port": "fl",
+    "state": "fl",
+    "price": "216"
+  },
+  {
+    "city": "Tampa",
+    "port": "fl",
+    "state": "fl",
+    "price": "158"
+  },
+  {
+    "city": "Tampa North",
+    "port": "fl",
+    "state": "fl",
+    "price": "194"
+  },
+  {
+    "city": "Atlanta",
+    "port": "ga",
+    "state": "ga",
+    "price": "161"
+  },
+  {
+    "city": "Atlanta East",
+    "port": "ga",
+    "state": "ga",
+    "price": "161"
+  },
+  {
+    "city": "Atlanta North",
+    "port": "ga",
+    "state": "ga",
+    "price": "161"
+  },
+  {
+    "city": "Atlanta South",
+    "port": "ga",
+    "state": "ga",
+    "price": "161"
+  },
+  {
+    "city": "Macon",
+    "port": "ga",
+    "state": "ga",
+    "price": "161"
+  },
+  {
+    "city": "Savannah",
+    "port": "ga",
+    "state": "ga",
+    "price": "61"
+  },
+  {
+    "city": "Tifton",
+    "port": "ga",
+    "state": "ga",
+    "price": "211"
+  },
+  {
+    "city": "Honolulu",
+    "port": "ca",
+    "state": "hi",
+    "price": "2040"
+  },
+  {
+    "city": "Davenport",
+    "port": "nj",
+    "state": "ia",
+    "price": "460"
+  },
+  {
+    "city": "Des Moines",
+    "port": "nj",
+    "state": "ia",
+    "price": "590"
+  },
+  {
+    "city": "Boise",
+    "port": "nj",
+    "state": "id",
+    "price": "461"
+  },
+  {
+    "city": "Chicago-North",
+    "port": "nj",
+    "state": "il",
+    "price": "390"
+  },
+  {
+    "city": "Chicago-South",
+    "port": "nj",
+    "state": "il",
+    "price": "415"
+  },
+  {
+    "city": "Chicago-West",
+    "port": "nj",
+    "state": "il",
+    "price": "415"
+  },
+  {
+    "city": "Lincoln",
+    "port": "nj",
+    "state": "il",
+    "price": "590"
+  },
+  {
+    "city": "St. Louis",
+    "port": "nj",
+    "state": "il",
+    "price": "538"
+  },
+  {
+    "city": "Fort Wayne",
+    "port": "nj",
+    "state": "in",
+    "price": "460"
+  },
+  {
+    "city": "Indianapolis",
+    "port": "nj",
+    "state": "in",
+    "price": "390"
+  },
+  {
+    "city": "South Bend",
+    "port": "nj",
+    "state": "in",
+    "price": "410"
+  },
+  {
+    "city": "Kansas City",
+    "port": "nj",
+    "state": "ks",
+    "price": "470"
+  },
+  {
+    "city": "Wichita",
+    "port": "nj",
+    "state": "ks",
+    "price": "520"
+  },
+  {
+    "city": "Ashland",
+    "port": "nj",
+    "state": "ky",
+    "price": "440"
+  },
+  {
+    "city": "Bowling Green",
+    "port": "ga",
+    "state": "ky",
+    "price": "391"
+  },
+  {
+    "city": "Lexington",
+    "port": "ga",
+    "state": "ky",
+    "price": "361"
+  },
+  {
+    "city": "Louisville",
+    "port": "ga",
+    "state": "ky",
+    "price": "391"
+  },
+  {
+    "city": "Louisville North",
+    "port": "ga",
+    "state": "ky",
+    "price": "391"
+  },
+  {
+    "city": "Paducah",
+    "port": "ga",
+    "state": "ky",
+    "price": "541"
+  },
+  {
+    "city": "Baton Rouge",
+    "port": "ga",
+    "state": "la",
+    "price": "341"
+  },
+  {
+    "city": "Lafayette",
+    "port": "ga",
+    "state": "la",
+    "price": "361"
+  },
+  {
+    "city": "New Orleans",
+    "port": "ga",
+    "state": "la",
+    "price": "331"
+  },
+  {
+    "city": "New Orleans East",
+    "port": "ga",
+    "state": "la",
+    "price": "331"
+  },
+  {
+    "city": "Shreveport",
+    "port": "ga",
+    "state": "la",
+    "price": "321"
+  },
+  {
+    "city": "Boston - Shirley",
+    "port": "nj",
+    "state": "ma",
+    "price": "180"
+  },
+  {
+    "city": "Taunton",
+    "port": "nj",
+    "state": "ma",
+    "price": "220"
+  },
+  {
+    "city": "Templeton",
+    "port": "nj",
+    "state": "ma",
+    "price": "345"
+  },
+  {
+    "city": "Baltimor",
+    "port": "nj",
+    "state": "md",
+    "price": "210"
+  },
+  {
+    "city": "Dundalk",
+    "port": "nj",
+    "state": "md",
+    "price": "220"
+  },
+  {
+    "city": "Laurel",
+    "port": "nj",
+    "state": "md",
+    "price": "260"
+  },
+  {
+    "city": "Metro DC",
+    "port": "nj",
+    "state": "md",
+    "price": "240"
+  },
+  {
+    "city": "Rosedale",
+    "port": "nj",
+    "state": "md",
+    "price": "220"
+  },
+  {
+    "city": "Portland - Gotham",
+    "port": "nj",
+    "state": "me",
+    "price": "580"
+  },
+  {
+    "city": "Detroit",
+    "port": "nj",
+    "state": "mi",
+    "price": "390"
+  },
+  {
+    "city": "Flint",
+    "port": "nj",
+    "state": "mi",
+    "price": "430"
+  },
+  {
+    "city": "Grand Rapids",
+    "port": "nj",
+    "state": "mi",
+    "price": "440"
+  },
+  {
+    "city": "Grand Lakes",
+    "port": "nj",
+    "state": "mi",
+    "price": "470"
+  },
+  {
+    "city": "Mineapolis / St.Paul",
+    "port": "nj",
+    "state": "mn",
+    "price": "540"
+  },
+  {
+    "city": "Springfield",
+    "port": "ga",
+    "state": "mo",
+    "price": "431"
+  },
+  {
+    "city": "Grenada",
+    "port": "ga",
+    "state": "ms",
+    "price": "371"
+  },
+  {
+    "city": "Gulf Coast",
+    "port": "ga",
+    "state": "ms",
+    "price": "341"
+  },
+  {
+    "city": "Billings",
+    "port": "ca",
+    "state": "mt",
+    "price": "1228"
+  },
+  {
+    "city": "Missoula",
+    "port": "nj",
+    "state": "mt",
+    "price": "990"
+  },
+  {
+    "city": "Ashville",
+    "port": "ga",
+    "state": "nc",
+    "price": "251"
+  },
+  {
+    "city": "Charlotte",
+    "port": "ga",
+    "state": "nc",
+    "price": "191"
+  },
+  {
+    "city": "Concord",
+    "port": "ga",
+    "state": "nc",
+    "price": "241"
+  },
+  {
+    "city": "Greensboro",
+    "port": "nj",
+    "state": "nc",
+    "price": "260"
+  },
+  {
+    "city": "Raleigh",
+    "port": "nj",
+    "state": "nc",
+    "price": "225"
+  },
+  {
+    "city": "Wilmington",
+    "port": "ga",
+    "state": "nc",
+    "price": "211"
+  },
+  {
+    "city": "Fargo",
+    "port": "nj",
+    "state": "nd",
+    "price": "820"
+  },
+  {
+    "city": "Omaha",
+    "port": "nj",
+    "state": "ne",
+    "price": "540"
+  },
+  {
+    "city": "Manchester",
+    "port": "nj",
+    "state": "nh",
+    "price": "300"
+  },
+  {
+    "city": "Avenel New Jersey",
+    "port": "nj",
+    "state": "nj",
+    "price": "154"
+  },
+  {
+    "city": "Central New Jersey",
+    "port": "nj",
+    "state": "nj",
+    "price": "80"
+  },
+  {
+    "city": "Northern New Jersey",
+    "port": "nj",
+    "state": "nj",
+    "price": "170"
+  },
+  {
+    "city": "Southern New Jersey",
+    "port": "nj",
+    "state": "nj",
+    "price": "130"
+  },
+  {
+    "city": "Albuquerque",
+    "port": "tx",
+    "state": "nm",
+    "price": "541"
+  },
+  {
+    "city": "Las Vegas",
+    "port": "ca",
+    "state": "nv",
+    "price": "560"
+  },
+  {
+    "city": "Reno",
+    "port": "ca",
+    "state": "nv",
+    "price": "620"
+  },
+  {
+    "city": "Albany",
+    "port": "nj",
+    "state": "ny",
+    "price": "220"
+  },
+  {
+    "city": "Buffalo",
+    "port": "nj",
+    "state": "ny",
+    "price": "340"
+  },
+  {
+    "city": "Long Island",
+    "port": "nj",
+    "state": "ny",
+    "price": "240"
+  },
+  {
+    "city": "Newburgh",
+    "port": "nj",
+    "state": "ny",
+    "price": "190"
+  },
+  {
+    "city": "Rochester",
+    "port": "nj",
+    "state": "ny",
+    "price": "410"
+  },
+  {
+    "city": "Syracuse",
+    "port": "nj",
+    "state": "ny",
+    "price": "327"
+  },
+  {
+    "city": "Acron-Canton",
+    "port": "nj",
+    "state": "oh",
+    "price": "470"
+  },
+  {
+    "city": "Cincinnati",
+    "port": "nj",
+    "state": "oh",
+    "price": "390"
+  },
+  {
+    "city": "Cleverland",
+    "port": "nj",
+    "state": "oh",
+    "price": "360"
+  },
+  {
+    "city": "Columbus",
+    "port": "nj",
+    "state": "oh",
+    "price": "370"
+  },
+  {
+    "city": "Dayton",
+    "port": "nj",
+    "state": "oh",
+    "price": "410"
+  },
+  {
+    "city": "Oklahoma City",
+    "port": "tx",
+    "state": "ok",
+    "price": "584"
+  },
+  {
+    "city": "Tulsa",
+    "port": "ga",
+    "state": "ok",
+    "price": "388"
+  },
+  {
+    "city": "Eugene",
+    "port": "ca",
+    "state": "or",
+    "price": "710"
+  },
+  {
+    "city": "Portland",
+    "port": "ca",
+    "state": "or",
+    "price": "710"
+  },
+  {
+    "city": "Portland West",
+    "port": "ca",
+    "state": "or",
+    "price": "660"
+  },
+  {
+    "city": "Altona",
+    "port": "nj",
+    "state": "pa",
+    "price": "310"
+  },
+  {
+    "city": "Bridgeport",
+    "port": "nj",
+    "state": "pa",
+    "price": "240"
+  },
+  {
+    "city": "Erie",
+    "port": "nj",
+    "state": "pa",
+    "price": "410"
+  },
+  {
+    "city": "Philadelphia",
+    "port": "nj",
+    "state": "pa",
+    "price": "160"
+  },
+  {
+    "city": "Philadelphia East",
+    "port": "nj",
+    "state": "pa",
+    "price": "210"
+  },
+  {
+    "city": "Pittsburg",
+    "port": "nj",
+    "state": "pa",
+    "price": "320"
+  },
+  {
+    "city": "Pittsburg-North",
+    "port": "nj",
+    "state": "pa",
+    "price": "320"
+  },
+  {
+    "city": "Scranton",
+    "port": "nj",
+    "state": "pa",
+    "price": "190"
+  },
+  {
+    "city": "York Springs",
+    "port": "nj",
+    "state": "pa",
+    "price": "210"
+  },
+  {
+    "city": "Providence",
+    "port": "nj",
+    "state": "ri",
+    "price": "260"
+  },
+  {
+    "city": "Charleston",
+    "port": "ga",
+    "state": "sc",
+    "price": "161"
+  },
+  {
+    "city": "Columbia",
+    "port": "ga",
+    "state": "sc",
+    "price": "243"
+  },
+  {
+    "city": "Greenville",
+    "port": "ga",
+    "state": "sc",
+    "price": "261"
+  },
+  {
+    "city": "Lexington",
+    "port": "ga",
+    "state": "sc",
+    "price": "323"
+  },
+  {
+    "city": "Sioux Falls",
+    "port": "nj",
+    "state": "sd",
+    "price": "760"
+  },
+  {
+    "city": "Chattanooga",
+    "port": "nj",
+    "state": "tn",
+    "price": "420"
+  },
+  {
+    "city": "Knoxville",
+    "port": "nj",
+    "state": "tn",
+    "price": "420"
+  },
+  {
+    "city": "Memphis",
+    "port": "ga",
+    "state": "tn",
+    "price": "371"
+  },
+  {
+    "city": "Nashville",
+    "port": "ga",
+    "state": "tn",
+    "price": "321"
+  },
+  {
+    "city": "Abilene",
+    "port": "tx",
+    "state": "tx",
+    "price": "471"
+  },
+  {
+    "city": "Amarillo",
+    "port": "tx",
+    "state": "tx",
+    "price": "511"
+  },
+  {
+    "city": "Austin",
+    "port": "tx",
+    "state": "tx",
+    "price": "341"
+  },
+  {
+    "city": "Corpus Christi",
+    "port": "tx",
+    "state": "tx",
+    "price": "391"
+  },
+  {
+    "city": "Dallas",
+    "port": "tx",
+    "state": "tx",
+    "price": "341"
+  },
+  {
+    "city": "Dalls / Ft. Worth",
+    "port": "tx",
+    "state": "tx",
+    "price": "361"
+  },
+  {
+    "city": "El Paso",
+    "port": "tx",
+    "state": "tx",
+    "price": "491"
+  },
+  {
+    "city": "Fort Worth North",
+    "port": "tx",
+    "state": "tx",
+    "price": "282"
+  },
+  {
+    "city": "Houston",
+    "port": "tx",
+    "state": "tx",
+    "price": "211"
+  },
+  {
+    "city": "Houston South",
+    "port": "tx",
+    "state": "tx",
+    "price": "329"
+  },
+  {
+    "city": "Houston North",
+    "port": "tx",
+    "state": "tx",
+    "price": "241"
+  },
+  {
+    "city": "Longview",
+    "port": "tx",
+    "state": "tx",
+    "price": "361"
+  },
+  {
+    "city": "Lubbock",
+    "port": "tx",
+    "state": "tx",
+    "price": "561"
+  },
+  {
+    "city": "McAllen",
+    "port": "tx",
+    "state": "tx",
+    "price": "361"
+  },
+  {
+    "city": "Permian Basin",
+    "port": "tx",
+    "state": "tx",
+    "price": "491"
+  },
+  {
+    "city": "San Antonio",
+    "port": "tx",
+    "state": "tx",
+    "price": "391"
+  },
+  {
+    "city": "San Antonio South",
+    "port": "tx",
+    "state": "tx",
+    "price": "361"
+  },
+  {
+    "city": "Salt Lake City",
+    "port": "ca",
+    "state": "ut",
+    "price": "660"
+  },
+  {
+    "city": "Culpeper",
+    "port": "nj",
+    "state": "va",
+    "price": "360"
+  },
+  {
+    "city": "Fredericksburg South",
+    "port": "nj",
+    "state": "va",
+    "price": "220"
+  },
+  {
+    "city": "Northern Virginia",
+    "port": "nj",
+    "state": "va",
+    "price": "296"
+  },
+  {
+    "city": "Pulaski",
+    "port": "nj",
+    "state": "va",
+    "price": "284"
+  },
+  {
+    "city": "Richmond",
+    "port": "nj",
+    "state": "va",
+    "price": "260"
+  },
+  {
+    "city": "Roanoke",
+    "port": "nj",
+    "state": "va",
+    "price": "400"
+  },
+  {
+    "city": "Suffolk",
+    "port": "nj",
+    "state": "va",
+    "price": "291"
+  },
+  {
+    "city": "Tidewater",
+    "port": "nj",
+    "state": "va",
+    "price": "320"
+  },
+  {
+    "city": "Burlington",
+    "port": "nj",
+    "state": "vt",
+    "price": "390"
+  },
+  {
+    "city": "Seattle",
+    "port": "ca",
+    "state": "wa",
+    "price": "852"
+  },
+  {
+    "city": "Appleton",
+    "port": "nj",
+    "state": "wi",
+    "price": "580"
+  },
+  {
+    "city": "IAA Ignite Auction Portage",
+    "port": "nj",
+    "state": "wi",
+    "price": "549"
+  },
+  {
+    "city": "Milwaukee",
+    "port": "nj",
+    "state": "wi",
+    "price": "410"
+  },
+  {
+    "city": "Buckhannon",
+    "port": "nj",
+    "state": "wv",
+    "price": "477"
+  },
+  {
+    "city": "Casper",
+    "port": "nj",
+    "state": "wy",
+    "price": "1099"
+  },
+];
---
+++ js/states.js
@@ -0,0 +1,308 @@
+const states = [
+  {
+    "Code_state": "AL",
+    "Name_state": "Alabama",
+    "Price_port": "475",
+    "Port": "SAV"
+  },
+  {
+    "Code_state": "AK",
+    "Name_state": "Alaska",
+    "Price_port": "1500",
+    "Port": "SEA"
+  },
+  {
+    "Code_state": "AZ",
+    "Name_state": "Arizona",
+    "Price_port": "400",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "AR",
+    "Name_state": "Arkansas",
+    "Price_port": "650",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "CA",
+    "Name_state": "California",
+    "Price_port": "350",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "CO",
+    "Name_state": "Colorado",
+    "Price_port": "650",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "CT",
+    "Name_state": "Connecticut",
+    "Price_port": "350",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "DE",
+    "Name_state": "Delaware",
+    "Price_port": "450",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "FL",
+    "Name_state": "Florida",
+    "Price_port": "250",
+    "Port": "MIA"
+  },
+  {
+    "Code_state": "GA",
+    "Name_state": "Georgia",
+    "Price_port": "350",
+    "Port": "SAV"
+  },
+  {
+    "Code_state": "HI",
+    "Name_state": "Hawaii",
+    "Price_port": "1600",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "ID",
+    "Name_state": "Idaho",
+    "Price_port": "500",
+    "Port": "SEA"
+  },
+  {
+    "Code_state": "IL",
+    "Name_state": "Illinois",
+    "Price_port": "275",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "IN",
+    "Name_state": "Indiana",
+    "Price_port": "350",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "IA",
+    "Name_state": "Iowa",
+    "Price_port": "450",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "KS",
+    "Name_state": "Kansas",
+    "Price_port": "475",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "KY",
+    "Name_state": "Kentucky",
+    "Price_port": "475",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "LA",
+    "Name_state": "Louisiana",
+    "Price_port": "475",
+    "Port": "HOU"
+  },
+  {
+    "Code_state": "ME",
+    "Name_state": "Maine",
+    "Price_port": "550",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "MD",
+    "Name_state": "Maryland",
+    "Price_port": "400",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "MA",
+    "Name_state": "Massachusetts",
+    "Price_port": "475",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "MI",
+    "Name_state": "Michigan",
+    "Price_port": "450",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "MN",
+    "Name_state": "Minnesota",
+    "Price_port": "425",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "MS",
+    "Name_state": "Mississippi",
+    "Price_port": "500",
+    "Port": "HOU"
+  },
+  {
+    "Code_state": "MO",
+    "Name_state": "Missouri",
+    "Price_port": "400",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "MT",
+    "Name_state": "Montana",
+    "Price_port": "850",
+    "Port": "SEA"
+  },
+  {
+    "Code_state": "NE",
+    "Name_state": "Nebraska",
+    "Price_port": "550",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "NV",
+    "Name_state": "Nevada",
+    "Price_port": "450",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "NH",
+    "Name_state": "New Hampshire",
+    "Price_port": "525",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "NJ",
+    "Name_state": "New Jersey",
+    "Price_port": "225",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "NM",
+    "Name_state": "New Mexico",
+    "Price_port": "600",
+    "Port": "HOU"
+  },
+  {
+    "Code_state": "NY",
+    "Name_state": "New York",
+    "Price_port": "400",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "NC",
+    "Name_state": "North Carolina",
+    "Price_port": "425",
+    "Port": "SAV"
+  },
+  {
+    "Code_state": "ND",
+    "Name_state": "North Dakota",
+    "Price_port": "550",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "OH",
+    "Name_state": "Ohio",
+    "Price_port": "525",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "OK",
+    "Name_state": "Oklahoma",
+    "Price_port": "500",
+    "Port": "HOU"
+  },
+  {
+    "Code_state": "OR",
+    "Name_state": "Oregon",
+    "Price_port": "325",
+    "Port": "SEA"
+  },
+  {
+    "Code_state": "PA",
+    "Name_state": "Pennsylvania",
+    "Price_port": "475",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "RI",
+    "Name_state": "Rhode Island",
+    "Price_port": "450",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "SC",
+    "Name_state": "South Carolina",
+    "Price_port": "375",
+    "Port": "SAV"
+  },
+  {
+    "Code_state": "SD",
+    "Name_state": "South Dakota",
+    "Price_port": "550",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "TN",
+    "Name_state": "Tennessee",
+    "Price_port": "475",
+    "Port": "SAV"
+  },
+  {
+    "Code_state": "TX",
+    "Name_state": "Texas",
+    "Price_port": "300",
+    "Port": "HOU"
+  },
+  {
+    "Code_state": "UT",
+    "Name_state": "Utah",
+    "Price_port": "550",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "VT",
+    "Name_state": "Vermont",
+    "Price_port": "450",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "VA",
+    "Name_state": "Virginia",
+    "Price_port": "500",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "WA",
+    "Name_state": "Washington",
+    "Price_port": "250",
+    "Port": "SEA"
+  },
+  {
+    "Code_state": "WV",
+    "Name_state": "West Virginia",
+    "Price_port": "550",
+    "Port": "NY"
+  },
+  {
+    "Code_state": "WI",
+    "Name_state": "Wisconsin",
+    "Price_port": "300",
+    "Port": "CHI"
+  },
+  {
+    "Code_state": "WY",
+    "Name_state": "Wyoming",
+    "Price_port": "600",
+    "Port": "LA"
+  },
+  {
+    "Code_state": "DC",
+    "Name_state": "Washington",
+    "Price_port": "400",
+    "Port": "NY"
+  }
+];
